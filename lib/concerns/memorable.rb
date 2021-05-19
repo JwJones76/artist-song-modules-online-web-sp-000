dcaac89395afb243994555a2c16698a740dc16bb
Jacob W Jones <jacobwadejones76@gmail.com>
2021-05-19 18:16:03 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,20 +1,20 @@
 require_relative '../lib/concerns/memorable'
 
-module Memorable
-  module ClassMethods
-  
-  def self.reset_all
-    self.all.clear
-  end
+ module Memorable
+   module ClassMethods
+   
+   def self.reset_all
+     self.all.clear
+   end
 
-  def self.count
-    self.all.count
-  end
-end
+   def self.count
+     self.all.count
+   end
+ end
 
-module InstanceMethods
-  def initialize
-    self.class.all << self
-  end
-end
+ module InstanceMethods
+   def initialize
+     self.class.all << self
+   end
+ end
 end
